21aa534a865afff7936dc6a6e9d8bfadc2df8ae9
Kaysera14 <131011220+Kaysera14@users.noreply.github.com>
2024-06-06 01:20:38 +0200

    created sidebar and integrated it with dashboard page

---
+++ src/components/sidebar.jsx
@@ -0,0 +1,59 @@
+import { Divider } from "@mui/material";
+import { useLogout } from "../hooks/use-logout";
+import { Logout } from "@mui/icons-material";
+
+export function Sidebar({ user, setModule }) {
+	const logout = useLogout();
+	const handleLogout = () => {
+		logout();
+	};
+	return (
+		<aside className="bg-slate-900 w-[18rem] h-screen border-r-2 border-gray-600 absolute flex flex-col">
+			<button onClick={() => alert("Soy un easter egg")}>
+				<img
+					src="/zenit-logo.webp"
+					alt="Zenit"
+					className="w-20 h-20 mx-auto my-5"
+				/>
+			</button>
+			<Divider
+				aria-hidden="true"
+				sx={{
+					"&::before, &::after": {
+						borderColor: "white",
+					},
+					textTransform: "uppercase",
+					fontSize: "1.2rem",
+				}}
+			>
+				<h1 className="flex justify-center">{user?.username}</h1>
+			</Divider>
+			<nav className="flex-grow">
+				<ul className="h-full flex flex-col gap-2">
+					<li>
+						<button
+							className="uppercase mx-auto w-full p-2 bg-slate-800 text-white hover:bg-slate-700"
+							onClick={() => setModule("home")}
+						>
+							Inicio
+						</button>
+					</li>
+					<li>
+						<button
+							className="uppercase mx-auto w-full p-2 bg-slate-800 text-white hover:bg-slate-700"
+							onClick={() => setModule("upload")}
+						>
+							Subir modelo
+						</button>
+					</li>
+				</ul>
+			</nav>
+			<button
+				className="uppercase text-xl flex items-center justify-center gap-2 w-full p-2 bg-red-500 text-white hover:bg-red-600 mt-auto"
+				onClick={handleLogout}
+			>
+				Cerrar sesión <Logout />
+			</button>
+		</aside>
+	);
+}
--- src/pages/dashboard.jsx
+++ src/pages/dashboard.jsx
@@ -1,10 +1,12 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { Main } from "../components/main";
 import { CurrentUserContext } from "../context/auth-context";
+import { Sidebar } from "../components/sidebar";
 
 export function Dashboard() {
 	const { user } = useContext(CurrentUserContext);
+	const [module, setModule] = useState("home");
 	const navigate = useNavigate();
 
 	useEffect(() => {
@@ -17,8 +19,12 @@ export function Dashboard() {
 	}, [user, navigate]);
 
 	return (
-		<Main>
-			<h1>Dashboard</h1>
-		</Main>
+		<>
+			<Sidebar user={user} setModule={setModule} />
+			<Main className={"ml-[18rem]"}>
+				{module === "home" ? <h1>Inicio</h1> : null}
+				{module === "upload" ? <h1>Subir modelo</h1> : null}
+			</Main>
+		</>
 	);
 }
