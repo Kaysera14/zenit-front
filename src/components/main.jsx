63ad193c71f4d7d56fb450fa764f0b293f87d6f2
Kaysera14 <131011220+Kaysera14@users.noreply.github.com>
2024-06-06 01:20:12 +0200

    added the posibility to add more styles

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,6 +1,8 @@
-export function Main({ children }) {
+export function Main({ children, className }) {
 	return (
-		<main className="flex flex-row justify-center flex-wrap gap-1 mb-8 md:gap-2">
+		<main
+			className={`flex flex-row justify-center flex-wrap gap-1 mb-8 md:gap-2 ${className}`}
+		>
 			{children}
 		</main>
 	);
