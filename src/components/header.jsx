cbc0df3565da50c1034c54be15f75d7509438394
Kaysera14 <131011220+Kaysera14@users.noreply.github.com>
2024-06-05 23:19:26 +0200

    added secret login hehe

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,20 @@
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 
 export function Header() {
 	const [isMobile, setIsMobile] = useState(false);
+	const [isDashboard, setIsDashboard] = useState(false);
+	const navigate = useNavigate();
+	const location = useLocation();
+
+	useEffect(() => {
+		const dashboard = () => {
+			if (location.pathname.startsWith("/admin/dashboard")) {
+				setIsDashboard(true);
+			}
+		};
+		dashboard();
+	}, [location.pathname]);
 
 	useEffect(() => {
 		const handleResize = () => {
@@ -13,42 +25,64 @@ export function Header() {
 			window.removeEventListener("resize", handleResize);
 		};
 	}, []);
-
+	const handleOnSubmit = (e) => {
+		e.preventDefault();
+		if (e.target[0].value === import.meta.env.VITE_APP_SECRET_KEY) {
+			e.target[0].value = "";
+			navigate("/admin/login");
+		} else {
+			e.target[0].value = "";
+			navigate("/");
+		}
+	};
 	return (
-		<header className="sm:h-[9rem] md:h-[12rem] w-full">
-			{!isMobile ? (
-				<>
-					<p className="flex justify-center pt-7 text-[4rem]">
-						<Link to={"/"}>Zenit Bragi</Link>
-					</p>
-					<nav>
-						<ul className="flex justify-center align-middle gap-4">
-							<li>
-								<button className="border-4 w-[148px] h-[40px] text-xl text-center">
-									Personal
-								</button>
-							</li>
-							<li>
-								<button className="border-4 w-[148px] h-[40px] text-xl text-center">
-									Professional
-								</button>
-							</li>
-						</ul>
-					</nav>
-				</>
-			) : (
-				<>
-					<p className="flex justify-center py-2 text-[4rem] border-b-[1px] border-b-white">
-						<Link to={"/"}>
-							<img
-								src="/zenit-logo.webp"
-								alt="Zenit Bragi"
-								className="h-[9rem] w-[9rem]"
-							/>
-						</Link>
-					</p>
-				</>
-			)}
-		</header>
+		<>
+			{!isDashboard ? (
+				<header className="sm:h-[9rem] md:h-[12rem] w-full">
+					{!isMobile ? (
+						<>
+							<form
+								onSubmit={handleOnSubmit}
+								className="h-[1.75rem] flex items-center justify-center"
+							>
+								<input
+									type="text"
+									className="w-[60px] h-[20px] text-xl text-center bg-transparent"
+								/>
+							</form>
+							<p className="flex justify-center pt-0 text-[4rem]">
+								<Link to={"/"}>Zenit Bragi</Link>
+							</p>
+							<nav>
+								<ul className="flex justify-center align-middle gap-4">
+									<li>
+										<button className="border-4 w-[148px] h-[40px] text-xl text-center">
+											Personal
+										</button>
+									</li>
+									<li>
+										<button className="border-4 w-[148px] h-[40px] text-xl text-center">
+											Professional
+										</button>
+									</li>
+								</ul>
+							</nav>
+						</>
+					) : (
+						<>
+							<p className="flex justify-center py-2 text-[4rem] border-b-[1px] border-b-white">
+								<Link to={"/"}>
+									<img
+										src="/zenit-logo.webp"
+										alt="Zenit Bragi"
+										className="h-[9rem] w-[9rem]"
+									/>
+								</Link>
+							</p>
+						</>
+					)}
+				</header>
+			) : null}
+		</>
 	);
 }
