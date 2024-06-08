dc6c210db4867493193954d2d66dbbc9a9154c2a
Kaysera0 <kaysera0@icloud.com>
2024-06-09 01:03:17 +0200

    better postrow

--- src/components/postrow.jsx
+++ src/components/postrow.jsx
@@ -1,10 +1,17 @@
 import { Delete, Edit } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 
 export function Postrow({ post, apiURL, handleDelete, handleEdit }) {
 	return (
-		<li className="flex flex-row w-full items-center justify-between">
+		<li className="flex flex-row w-full items-center justify-around">
 			<p>{post?.model_id}</p>
-			<img src={apiURL + post?.cover} alt={post?.slug} className="w-20 h-20" />
+			<Link to={`/models/${post?.slug}`}>
+				<img
+					src={apiURL + post?.cover}
+					alt={post?.slug}
+					className="w-20 h-20 static object-cover"
+				/>
+			</Link>
 			<p>{post?.title}</p>
 			<p>{post?.category1}</p>
 			<p>{post?.category2}</p>
