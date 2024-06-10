bfa43f350f0c036ca3713e0d9299ae9821b7050b
Kaysera0 <kaysera0@icloud.com>
2024-06-10 05:03:13 +0200

    fixed another bug in the topbar

--- src/components/topbar.jsx
+++ src/components/topbar.jsx
@@ -1,6 +1,6 @@
 export function Topbar() {
 	return (
-		<header className="bg-slate-900 h-[8.45rem] flex justify-center items-center border-b-[1px] border-white absolute w-screen pl-[18rem]">
+		<header className="bg-slate-900 h-[8.45rem] flex justify-center items-center border-b-[1px] border-white absolute w-[100%] pl-[18rem]">
 			<h1 className="text-2xl font-bold uppercase">Panel de control</h1>
 		</header>
 	);
