e0593b8848bea335e0cbcaa917044872d401e864
Kaysera0 <kaysera0@icloud.com>
2024-06-10 04:46:45 +0200

    fixed size

--- src/components/topbar.jsx
+++ src/components/topbar.jsx
@@ -1,6 +1,6 @@
 export function Topbar() {
 	return (
-		<header className="bg-slate-900 ml-[18rem] h-[8.45rem] flex justify-center items-center border-b-[1px] border-white">
+		<header className="bg-slate-900 h-[8.45rem] flex justify-center items-center border-b-[1px] border-white absolute w-screen pl-[18rem]">
 			<h1 className="text-2xl font-bold uppercase">Panel de control</h1>
 		</header>
 	);
