cd8a8586941f5758535e5f2894e0d36acd5e739b
Kaysera0 <kaysera0@icloud.com>
2024-06-11 18:05:47 +0200

    fetching now in app

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,35 +1,7 @@
-import { useState } from "react";
 import { Main } from "../components/main";
-import { useEffect } from "react";
-import { getPosts } from "../api/get-posts.js";
 import { PostCard } from "../components/postcard";
 
-export function Home({ filter }) {
-	const [posts, setPosts] = useState([]);
-	const [filteredPosts, setFilteredPosts] = useState([]);
-
-	useEffect(() => {
-		const fetchPosts = async () => {
-			const posts = await getPosts();
-			const postsData = posts.data;
-			setPosts(postsData);
-		};
-
-		if (filter === "Professional" || filter === "Personal") {
-			const newFilteredPosts = posts.filter((post) =>
-				post.category1.toLowerCase().includes(filter.toLowerCase())
-			);
-			setFilteredPosts(newFilteredPosts);
-		} else {
-			if (posts.length === 0) {
-				fetchPosts();
-			}
-		}
-
-		if (filter === "") {
-			setFilteredPosts([]);
-		}
-	}, [filter, posts]);
+export function Home({ filteredPosts, posts }) {
 	return (
 		<Main>
 			{filteredPosts.length > 0
