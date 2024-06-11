3f0ca157a85988ad6cc3e57a6ce8c935acc30fa7
Kaysera0 <kaysera0@icloud.com>
2024-06-11 21:01:41 +0200

    added footer and modified main

---
+++ src/components/footer.jsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import SimpleIconsArtstation from "../assets/SimpleIconsArtstation";
+import SimpleIconsLinkedin from "../assets/SimpleIconsLinkedin";
+import SimpleIconsInstagram from "../assets/SimpleIconsInstagram";
+
+export function Footer() {
+	const [isDashboard, setIsDashboard] = useState(false);
+	const location = useLocation();
+
+	useEffect(() => {
+		if (location.pathname.startsWith("/admin/dashboard")) {
+			setIsDashboard(true);
+		} else {
+			setIsDashboard(false);
+		}
+	}, [location?.pathname]);
+
+	return (
+		<>
+			{!isDashboard ? (
+				<footer className="flex flex-col items-center text-center bg-[#0e0c0a] text-color border-t-[1px] border-gray-500 fixed bottom-0 w-full h-[4.5rem]">
+					<section className="flex justify-center gap-2 pt-3">
+						<Link to="https://www.artstation.com/zenitbragi">
+							<SimpleIconsArtstation />
+						</Link>
+						<Link to="https://www.linkedin.com/in/ariadna-aguilera-93a552267/">
+							<SimpleIconsLinkedin />
+						</Link>
+						<Link to="https://www.instagram.com/zenit.bragi/">
+							<SimpleIconsInstagram />
+						</Link>
+					</section>
+					<section className="w-full text-center pt-4">
+						©{new Date().getFullYear()} Zenit Bragi
+					</section>
+				</footer>
+			) : null}
+		</>
+	);
+}
--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,7 +1,7 @@
 export function Main({ children, className }) {
 	return (
 		<main
-			className={`flex flex-row justify-center flex-wrap gap-1 mb-8 md:gap-2 ${className}`}
+			className={`flex flex-row justify-center flex-wrap gap-1 mb-8 pb-[4.5rem] md:gap-2 ${className}`}
 		>
 			{children}
 		</main>
