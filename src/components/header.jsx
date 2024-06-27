5accc18fd75bd5513db0f92ef34436d2f6b38523
Kaysera0 <kaysera0@icloud.com>
2024-06-27 03:02:49 +0200

    fixed header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -16,19 +16,6 @@ export function Header({ setFilter, filter }) {
 		}
 	}, [location?.pathname]);
 
-	useEffect(() => {
-		const handleResize = () => {
-			setIsMobile(window.innerWidth < 768);
-		};
-
-		handleResize();
-
-		window.addEventListener("resize", handleResize);
-		return () => {
-			window.removeEventListener("resize", handleResize);
-		};
-	}, []);
-
 	const handleOnSubmit = (e) => {
 		e.preventDefault();
 		if (e.target[0].value === import.meta.env.VITE_APP_SECRET_KEY) {
@@ -42,125 +29,76 @@ export function Header({ setFilter, filter }) {
 	return (
 		<>
 			{!isDashboard ? (
-				<header className="sm:h-[9rem] md:h-[12rem] w-full">
-					{!isMobile ? (
-						<>
-							<form
-								onSubmit={handleOnSubmit}
-								className="h-[1.75rem] flex items-center justify-center"
-							>
-								<input
-									type="text"
-									className="w-[75px] h-[20px] text-xl text-center bg-transparent"
+				<header className="h-[12rem] w-full">
+					<form
+						onSubmit={handleOnSubmit}
+						className="h-[1.75rem] hidden md:flex items-center justify-center"
+					>
+						<input
+							type="text"
+							className="w-[75px] h-[20px] text-xl text-center bg-transparent"
+						/>
+					</form>
+					<p className="hidden md:flex justify-center pt-0 text-[4rem]">
+						<Link onClick={() => setFilter("")} to={"/"}>
+							Zenit Bragi
+						</Link>
+					</p>
+					<section className="border-b-[1px] border-b-white pb-2 md:border-0">
+						<p className="flex justify-center md:hidden py-2 text-[4rem]">
+							<Link onClick={() => setFilter("")} to={"/"}>
+								<img
+									src="/zenit-logo.webp"
+									alt="Zenit Bragi"
+									className="h-[6.4rem] w-[6.4rem]"
 								/>
-							</form>
-							<p className="flex justify-center pt-0 text-[4rem]">
-								<Link onClick={() => setFilter("")} to={"/"}>
-									Zenit Bragi
-								</Link>
-							</p>
-							<nav>
-								<ul className="flex justify-center align-middle gap-4">
-									<li>
-										<Button
-											onClick={() => setFilter("Personal")}
-											sx={{
-												border: "4px solid",
-												width: "148px",
-												height: "40px",
-												fontSize: "1.25rem",
-												textAlign: "center",
-												textTransform: "capitalize",
-												color: "#e5e7eb",
-												fontWeight: "normal",
-												borderRadius: "0",
-											}}
-											variant={filter === "Personal" ? "contained" : "outlined"}
-										>
-											Personal
-										</Button>
-									</li>
-									<li>
-										<Button
-											onClick={() => setFilter("Professional")}
-											sx={{
-												border: "4px solid",
-												width: "148px",
-												height: "40px",
-												fontSize: "1.25rem",
-												textAlign: "center",
-												textTransform: "capitalize",
-												color: "#e5e7eb",
-												fontWeight: "normal",
-												borderRadius: "0",
-											}}
-											variant={
-												filter === "Professional" ? "contained" : "outlined"
-											}
-										>
-											Professional
-										</Button>
-									</li>
-								</ul>
-							</nav>
-						</>
-					) : (
-						<section className="border-b-[1px] border-b-white pb-2">
-							<p className="flex justify-center py-2 text-[4rem]">
-								<Link onClick={() => setFilter("")} to={"/"}>
-									<img
-										src="/zenit-logo.webp"
-										alt="Zenit Bragi"
-										className="h-[6.4rem] w-[6.4rem]"
-									/>
-								</Link>
-							</p>
-							<nav>
-								<ul className="flex justify-center align-middle gap-4">
-									<li>
-										<Button
-											onClick={() => setFilter("Personal")}
-											sx={{
-												border: "4px solid",
-												width: "148px",
-												height: "40px",
-												fontSize: "1.25rem",
-												textAlign: "center",
-												textTransform: "capitalize",
-												color: "#e5e7eb",
-												fontWeight: "normal",
-												borderRadius: "0",
-											}}
-											variant={filter === "Personal" ? "contained" : "outlined"}
-										>
-											Personal
-										</Button>
-									</li>
-									<li>
-										<Button
-											onClick={() => setFilter("Professional")}
-											sx={{
-												border: "4px solid",
-												width: "148px",
-												height: "40px",
-												fontSize: "1.25rem",
-												textAlign: "center",
-												textTransform: "capitalize",
-												color: "#e5e7eb",
-												fontWeight: "normal",
-												borderRadius: "0",
-											}}
-											variant={
-												filter === "Professional" ? "contained" : "outlined"
-											}
-										>
-											Professional
-										</Button>
-									</li>
-								</ul>
-							</nav>
-						</section>
-					)}
+							</Link>
+						</p>
+						<nav>
+							<ul className="flex justify-center align-middle gap-4">
+								<li>
+									<Button
+										onClick={() => setFilter("Personal")}
+										sx={{
+											border: "4px solid",
+											width: "148px",
+											height: "40px",
+											fontSize: "1.25rem",
+											textAlign: "center",
+											textTransform: "capitalize",
+											color: "#e5e7eb",
+											fontWeight: "normal",
+											borderRadius: "0",
+										}}
+										variant={filter === "Personal" ? "contained" : "outlined"}
+									>
+										Personal
+									</Button>
+								</li>
+								<li>
+									<Button
+										onClick={() => setFilter("Professional")}
+										sx={{
+											border: "4px solid",
+											width: "148px",
+											height: "40px",
+											fontSize: "1.25rem",
+											textAlign: "center",
+											textTransform: "capitalize",
+											color: "#e5e7eb",
+											fontWeight: "normal",
+											borderRadius: "0",
+										}}
+										variant={
+											filter === "Professional" ? "contained" : "outlined"
+										}
+									>
+										Professional
+									</Button>
+								</li>
+							</ul>
+						</nav>
+					</section>
 				</header>
 			) : null}
 		</>
